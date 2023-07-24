541a9555ef196c2b2fe9fa8a7843d4ebe1c5cbb2
TommasoBertossi <tommasobertossi@gmail.com>
2023-07-24 09:23:03 +0200

    fix simbol bug

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 let gameNumber;
 
 for(let i = 1; i <= 100; i++) {
-    if (i & 15 == 0) {
-        gameNumber = ("FizzBuzz!");
-    } else if (i & 3 == 0) {
-        gameNumber = ("Fizz")
-    } else if (i & 5 == 0) {
-        gameNumber = ("Buzz")
+    if (i % 15 == 0) {
+        gameNumber = "FizzBuzz!";
+    } else if (i % 3 == 0) {
+        gameNumber = "Fizz"
+    } else if (i % 5 == 0) {
+        gameNumber = "Buzz"
     } else {
         gameNumber = i;
     }
